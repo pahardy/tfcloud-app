01a18647d3285ecb45abfeed4ffc97042be35329
root <hardy_patrick@yahoo.ca>
2024-02-04 08:27:19 -0500

    add provider to main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 resource "aws_instance" "terraform-cloud" {
   ami = "ami-0a2e7efb4257c0907"
   instance_type = "t2.micro"
