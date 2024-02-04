cb8199f7adb8584f02888c2e0cbdf8b070931fa2
root <hardy_patrick@yahoo.ca>
2024-02-04 08:20:26 -0500

    add main.tf file

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "terraform-cloud" {
+  ami = "ami-0a2e7efb4257c0907"
+  instance_type = "t2.micro"
+  security_groups = ["SSH-from-all"]
+  key_name = "MyKeyPair"
+
+  tags = {
+    Name = var.environment
+  }
+}
